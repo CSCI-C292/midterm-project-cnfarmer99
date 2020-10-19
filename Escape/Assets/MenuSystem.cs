3cc03ce0db2d9fcf3dce170ad822a7617ae7d923
cnfarmer99 <54722585+cnfarmer99@users.noreply.github.com>
2020-10-18 22:04:07 -0400

    Menus are added.  Editing cosmetics

---
+++ Escape/Assets/MenuSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuSystem : MonoBehaviour
+{
+    public void playClicked()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+
+    public void instructionsClicked()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+
+    public void quitClicked()
+    {
+        Application.Quit();
+    }
+
+    public void backClicked()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
