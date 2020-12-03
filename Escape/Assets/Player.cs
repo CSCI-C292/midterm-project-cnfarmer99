f7071b6ecf34fe080a1478d334f3b31bcfc347c2
cnfarmer99 <54722585+cnfarmer99@users.noreply.github.com>
2020-12-02 23:28:50 -0500

    I'm frustrated and it's broken

---
+++ Escape/Assets/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float _moveSpeed = 3f;
+
+    void Movement()
+    {
+        Vector3 sidewaysMovementVector =  transform.right * Input.GetAxis("up1");
+        Vector3 forwardAndBackwardMovementVector = transform.forward * Input.GetAxis("down1");
+        Vector3 movementVector = sidewaysMovementVector + forwardAndBackwardMovementVector;
+        
+        GetComponent<CharacterController>().Move(movementVector * _moveSpeed * Time.deltaTime);
+    }
+}
